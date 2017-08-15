93312e13f889539b8aa23dc7b7d70e0ccaaa1d48
Manjari Narayan (SU) <mnarayan@users.noreply.github.com>
2017-08-14 18:10:16 -0700

    Added header files

---
+++ fastclime/dantzig.h
@@ -0,0 +1,1 @@
+void dantzig(double *X2, double *Xy, double *BETA0, int *d0, double *lambda, int *nlambda, double *lambdalist);
---
+++ fastclime/parametric.h
@@ -0,0 +1,3 @@
+int ratio_test(double *dy, int *idy,int ndy, double *y, double mu);
+void parametric(double *SigmaInput, int *m1, double *mu_input, double *lambdamin, int *nlambda, int *maxnlambda, double *iicov);
+void solver2(int m,int n,int nz,int *ia, int *ka, double *a,double *b, double *c);
