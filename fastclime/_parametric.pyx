01d103d2ee9ca61d18c0b61b3dc812e9719e68dd
Manjari Narayan (SU) <mnarayan@users.noreply.github.com>
2017-08-14 18:09:58 -0700

    Added cython wrapper for clime

---
+++ fastclime/_parametric.pyx
@@ -0,0 +1,41 @@
+import numpy as np
+cimport numpy as cnp
+
+cnp.import_array()
+
+cdef extern from "parametric.h":
+    void parametric(double *Shat, int *p, 
+                    double *mu_input, 
+                    double *lambdamin, 
+                    int *nlambda, 
+                    int *maxnlambda, 
+                    double *iicov
+                    );
+    
+def mainfunc(cnp.ndarray[double, ndim=2, mode="c"] Shat not None,
+            cnp.ndarray[double, ndim=2, mode="c"] mu_input,
+            double lambdamin,
+            int nlambda,
+            int maxnlambda,
+            cnp.ndarray[double, ndim=2, mode="c"] iicov,
+            ):
+    
+    cdef int p = Shat.shape[1]
+    # cdef cnp.ndarray mu_input = cnp.zeros((p,nlambda),
+    #                                     dtype = cnp.float64,
+    #                                     order="C"
+    #                                     )
+    # cdef cnp.ndarray iicov = cnp.zeros((p*p,nlambda),
+    #                                    dtype = cnp.float64,
+    #                                    order="C"
+    #                                    )
+    parametric(<double*> Shat.data,
+               &p,
+               <double*> mu_input.data,
+               <double*> &lambdamin,
+               &nlambda,
+               &maxnlambda,
+               <double*> iicov.data
+              )
+
+    return (Shat,mu_input,maxnlambda,iicov)
